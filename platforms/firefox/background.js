eed944afdac0073091a8dfbc788207ab62d87a92
mastermajorman <devinmochrie@gmail.com>
2021-11-17 15:35:41 -0500

    compartmentalization of platforms in main branch

---
+++ platforms/firefox/background.js
@@ -0,0 +1,30 @@
+chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
+    if (changeInfo.status === 'complete' && /^https:\/\/www\.youtube.com\/watch\?v=.*/.test(tab.url)) {
+        chrome.tabs.executeScript(null, {file: './foreground.js'}, () => {});
+    }
+});
+
+chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+    if (request.message === 'get_dislikes') {
+        chrome.tabs.query({active: true, currentWindow: true}, tabs => {
+            let url = tabs[0].url;
+            // accounts for https://www.youtube.com/ (length 25), additional check
+            if (url.length > 25) {
+                let youtubeId = url.split("v=")[1].substring(0,11);
+                fetch(`https://s7vueecr2e.execute-api.us-east-1.amazonaws.com/prod?videoId=${youtubeId}`)
+                .then(response => {
+                    // indicates whether the response is successful (status code 200-299) or not
+                    if (!response.ok) {
+                    throw new Error(`Request failed with status ${reponse.status}`)
+                    }
+                    return response.json()
+                })
+                .then(data => {
+                    sendResponse({ message: data });
+                })
+                .catch(error => console.log(error))
+            }
+        });
+        return true;
+    }
+});
---
+++ platforms/firefox/foreground.js
@@ -0,0 +1,46 @@
+chrome.runtime.sendMessage({
+    message: "get_dislikes"
+}, response => {
+    // don't want to have to do this, but w/e
+    setTimeout(function () {
+        // setup
+        let dislikeCount = response.message.items[0].statistics.dislikeCount;
+
+        let countArea = document.getElementsByClassName("style-scope ytd-menu-renderer force-icon-button")[1].childNodes[0].childNodes[1];
+        countArea.innerText = intToString(dislikeCount);
+
+        let dislikeButton = document.getElementsByClassName("style-scope ytd-menu-renderer force-icon-button")[1].childNodes[0].childNodes[0].childNodes[1];
+
+        // local value should change +1/-1
+        dislikeButton.addEventListener("click", function () {
+            if (dislikeButton.getAttribute("aria-pressed") === "false") {
+                dislikeCount ++;
+            } else {
+                dislikeCount--;
+            }
+            countArea.innerText = intToString(dislikeCount);
+        });
+    }, 500);
+});
+
+function intToString(num) {
+    num = num.toString().replace(/[^0-9.]/g, '');
+    if (num < 1000) {
+        return num;
+    }
+    let si = [
+      {v: 1E3, s: "K"},
+      {v: 1E6, s: "M"},
+      {v: 1E9, s: "B"},
+      {v: 1E12, s: "T"},
+      {v: 1E15, s: "P"},
+      {v: 1E18, s: "E"}
+      ];
+    let index;
+    for (index = si.length - 1; index > 0; index--) {
+        if (num >= si[index].v) {
+            break;
+        }
+    }
+    return (num / si[index].v).toFixed(1).replace(/\.0+$|(\.[0-9]*[1-9])0+$/, "$1") + si[index].s;
+}
